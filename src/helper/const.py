d8ae96d1e3badc4f657528ad2e8f78f99049ab4e
Thomas Schremser <thomas.schremser@t-systems.com>
2021-03-15 03:36:23 +0100

    Random Guesses

---
+++ src/helper/const.py
@@ -0,0 +1,9 @@
+SEED = 42
+
+GENRE_OHE_COLS = [
+  'x0_Action', 'x0_Adventure', 'x0_Animation', 'x0_Comedy',
+  'x0_Crime', 'x0_Documentary', 'x0_Drama', 'x0_Family',
+  'x0_Fantasy','x0_History', 'x0_Horror', 'x0_Music', 'x0_Mystery',
+  'x0_Romance', 'x0_Science Fiction', 'x0_TV Movie', 'x0_Thriller',
+  'x0_War','x0_Western'
+]
--- src/helper/helper.py
+++ src/helper/helper.py
@@ -4,6 +4,8 @@ That module contains all helper functions of the project
 import pandas as pd
 import multiprocessing
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def parallelize_dataframe(df: pd.DataFrame, func: object, num_cpu: int = 0) -> pd.DataFrame:
     '''
@@ -39,3 +41,42 @@ def parallelize_dataframe(df: pd.DataFrame, func: object, num_cpu: int = 0) -> p
     pool.close()
     pool.join()
     return df
+
+
+def plot_correlations(df: pd.DataFrame, num_cols: list = None, show_diag: bool = True, figsize=(8, 5)):
+    """Plot the correlation matrix using Pearson."""
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+    mask = np.triu(np.ones_like(df[num_cols].corr(), dtype=np.bool))
+
+    if isinstance(num_cols, type(None)):
+        num_cols = [c for c in df.columns if pd.api.types.is_numeric_dtype(df[c])]
+
+    _, ax = plt.subplots(nrows=1, ncols=1, figsize=figsize)
+    ax.set_title("Correlation of columns")
+    if show_diag:
+        sns.heatmap(
+            df[num_cols].corr(), cmap=cmap, vmin=-1, vmax=1, annot=True, ax=ax)
+    else:
+        sns.heatmap(
+            df[num_cols].corr(), cmap=cmap, vmin=-1, vmax=1, annot=True, mask=mask, ax=ax)
+
+    plt.show()
+
+
+def get_dist_of_simple_genre_combis(df: pd.DataFrame, genre_cols: list, verbose: bool = False) -> list:
+    """
+    Get number of genres per simple genre combinations
+    """
+    # Calc distibution of one or multi genres per movie
+    l_rowsum = []
+    l_count_gen = []
+    for idx, row in df.iterrows():
+        l_rowsum.append(row[genre_cols].sum())
+
+    gen_dist = np.array(l_rowsum)
+    for i in range(0, len(genre_cols)):
+        count = len(np.where(gen_dist==i)[0])
+        l_count_gen.append(count)
+        if verbose:
+            print(f"Number of movies holding {i} genres: {count}")
+    return l_count_gen
