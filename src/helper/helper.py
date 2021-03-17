ca61e0735ab64ce7864e499accf442534b3f3397
Thomas Schremser <thomas.schremser@t-systems.com>
2021-03-17 12:51:40 +0100

    Backup

--- src/helper/helper.py
+++ src/helper/helper.py
@@ -8,7 +8,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-# Helper function
 def parallelize_dataframe(df: pd.DataFrame, func: object, num_cpu: int = 0) -> pd.DataFrame:
     '''
     Process a dataframe using multiprocessing
