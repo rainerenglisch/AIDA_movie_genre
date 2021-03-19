9fa1d2acbb31c4ae35898bc936fb9e13e51f9b67
Thomas Schremser <thomas.schremser@t-systems.com>
2021-03-20 00:26:20 +0100

    Update

--- src/helper/helper.py
+++ src/helper/helper.py
@@ -6,7 +6,30 @@ import multiprocessing
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from keras import backend as K
 
+
+def recall_m(y_true, y_pred):
+    """Calculate recall"""
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def precision_m(y_true, y_pred):
+    """Calculate precision"""
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def f1_m(y_true, y_pred):
+    """Calcuates F1 metric"""
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 def parallelize_dataframe(df: pd.DataFrame, func: object, num_cpu: int = 0) -> pd.DataFrame:
     '''
